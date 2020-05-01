eddcacc3141c3f3fa7ee38d2ba93790177bab87e
Almesberger Marcel <marcel.almesberger@gmail.com>
2020-05-01 18:56:39 +0200

    add chart.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,25 @@
-// Check if DOM is loaded
+//load required modules
+const Chart = require("chart.js")
+
+// check if DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM has been loaded and ready!");
 
+    // GENERATE LINE GRAPH FOR OVERVIEW PAGE
 
+    // Graph data
+    let graphData;
+
+    // Select <canvas> element
+    let ctx = document.getElementById("myChart");
+
+    //create line graph
+    let lineChart = new Chart(ctx, {
+        type: "line",
+        data: graphData
+
+
+    });
 
 
     
