8afbc1db0ffdfcb68ac0f6dbf76e6f7cab4c2daf
Almesberger Marcel <marcel.almesberger@gmail.com>
2020-05-03 13:35:45 +0200

    integration Chart.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -11,6 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // Select <canvas> element
     let ctx = document.getElementById("myChart");
 
+    // Update global variables of Chart.js
+    Chart.defaults.global.defaultFontColor = "#03051C";
+    Chart.defaults.global.defaultFontSize = 20;
+    Chart.defaults.global.defaultFontFamily = "'Roboto', 'sans-serif'";
+
     //create line graph
     let lineChart = new Chart(ctx, {
         type: "line",
@@ -18,6 +23,16 @@ document.addEventListener("DOMContentLoaded", () => {
             labels: graphLabels,
             datasets: [{
                 label: "INR",
+                // line colours
+                backgroundColor: "#3460EF",
+                borderColor: "#3460EF",
+                // filling area under the line
+                fill: false,
+                // point colours
+                pointBackgroundColor: "#03051C",
+                pointBorderColor: "#03051C",
+                // point size
+                pointRadius: 4,
                 data: graphData
             }]
         },
