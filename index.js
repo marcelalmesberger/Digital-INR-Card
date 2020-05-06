f43db857b48ff51602115d85935338fd3c57a352
Almesberger Marcel <marcel.almesberger@gmail.com>
2020-05-06 08:02:37 +0200

    implement settings change

--- index.js
+++ index.js
@@ -24,52 +24,67 @@ app.get("/", (req, res) => {
 
 // registration first step
 app.get("/public/pages/registration1.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/registration1.html")
+    res.sendFile(__dirname + "/public/pages/registration1.html");
 });
 
 // registration second step
 app.get("/public/pages/registration2.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/registration2.html")
+    res.sendFile(__dirname + "/public/pages/registration2.html");
 });
 
 // registration third step
 app.get("/public/pages/registration3.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/registration3.html")
+    res.sendFile(__dirname + "/public/pages/registration3.html");
 });
 
 // overview page
 app.get("/public/pages/overview.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/overview.html")
+    res.sendFile(__dirname + "/public/pages/overview.html");
 });
 
 // my values page
 app.get("/public/pages/myvalues.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/myvalues.html")
+    res.sendFile(__dirname + "/public/pages/myvalues.html");
 });
 
 // import & export page
 app.get("/public/pages/importexport.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/importexport.html")
+    res.sendFile(__dirname + "/public/pages/importexport.html");
 });
 
 // food page
 app.get("/public/pages/food.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/food.html")
+    res.sendFile(__dirname + "/public/pages/food.html");
 });
 
 // interactions page
 app.get("/public/pages/interactions.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/interactions.html")
+    res.sendFile(__dirname + "/public/pages/interactions.html");
 });
 
 // settings page
 app.get("/public/pages/settings.html", (req, res) => {
-    res.sendFile(__dirname + "/public/pages/settings.html")
+    res.sendFile(__dirname + "/public/pages/settings.html");
 });
 
 // landing page after logout
 app.get("/public/pages/index.html", (req, res) => {
     res.redirect("/");
+});
+
+// helper page change access data
+app.get("/public/pages/helpers/changeaccess.html", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/helpers/changeaccess.html");
+});
+
+// helper page change personal data
+app.get("/public/pages/helpers/changepersonal.html", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/helpers/changepersonal.html");
+});
+
+// helper page change medication data
+app.get("/public/pages/helpers/changemedication.html", (req, res) => {
+    res.sendFile(__dirname + "/public/pages/helpers/changemedication.html");
 });
 
 /* POST-REQUESTS */
@@ -190,6 +205,85 @@ app.post("/public/pages/importexport.html/submit", upload.single("fileupload"), 
             });
         })
     res.redirect("/public/pages/importexport.html");
+});
+
+// change access data
+app.post("/public/pages/helpers/changeaccess.html/submit", (req, res) => {
+    // load settings.json
+    let rawSettings = fs.readFileSync("public/data/settings.json");
+    // parse settings.json
+    let settings = JSON.parse(rawSettings);
+    console.log(settings);
+    // save Email in variable and update settings.json
+    console.log("Email: " + req.body.regemail);
+    let email = req.body.regemail;
+    settings.email = email;
+    // save Password in variable and update settings.json
+    console.log("Passwort: " + req.body.regpwd);
+    let pwd = req.body.regpwd;
+    settings.password = pwd;
+    // save Check-Password in variable (not used yet in this prototype!)
+    console.log("Bestätigung Passwort: " + req.body.checkpwd);
+    let checkpwd = req.body.checkpwd;
+    // save updated settings object in JSON
+    console.log(settings);
+    fs.writeFileSync("public/data/settings.json", JSON.stringify(settings));
+    // redirect to settings site
+    res.redirect("/public/pages/settings.html");
+});
+
+// change personal data
+app.post("/public/pages/helpers/changepersonal.html/submit", (req, res) => {
+    // load settings.json
+    let rawSettings = fs.readFileSync("public/data/settings.json");
+    // parse settings.json
+    let settings = JSON.parse(rawSettings);
+    console.log(settings);
+    // save Surname in varibale and update settings.json
+    console.log("Nachname: " + req.body.surname);
+    let surname = req.body.surname;
+    settings.surname = surname;
+    // save Prename in variable and update settings.json
+    console.log("Vorname: " + req.body.prename);
+    let prename = req.body.prename;
+    settings.prename = prename;
+    // save Birthdate in variable and update settings.json
+    console.log("Geburtsdatum: " + req.body.birthdate);
+    let birthdate = req.body.birthdate;
+    settings.birthdate = birthdate
+    // save updated settings object in JSON
+    console.log(settings);
+    fs.writeFileSync("public/data/settings.json", JSON.stringify(settings));
+    // redirect to settings site
+    res.redirect("/public/pages/settings.html");
+});
+
+// change medication data
+app.post("/public/pages/helpers/changemedication.html/submit", (req, res) => {
+    // load settings.json
+    let rawSettings = fs.readFileSync("public/data/settings.json");
+    // parse settings.json
+    let settings = JSON.parse(rawSettings);
+    console.log(settings);
+    // save Medication in variable an update settings.json
+    console.log("Medikament: " + req.body.medication);
+    let medication = req.body.medication;
+    settings.medication = medication;
+    // save INR Target Value in variable and update settings.json
+    console.log("INR-Zielwert " + req.body.targetvalue);
+    let targetValue = req.body.targetvalue;
+    settings.targetValue = targetValue;
+    // save INR Target Range in variables and update settings.json
+    console.log("INR-Zielbereich: von " + req.body.minrange + " bis " + req.body.maxrange);
+    let minRange = req.body.minrange;
+    let maxRange = req.body.maxrange;
+    settings.minRange = minRange;
+    settings.maxRange = maxRange;
+    // save updated settings object in JSON
+    console.log(settings);
+    fs.writeFileSync("public/data/settings.json", JSON.stringify(settings));
+    // redirect to settings site
+    res.redirect("/public/pages/settings.html");
 });
 
 // Server listening on port 3000, http://localhost:3000/
