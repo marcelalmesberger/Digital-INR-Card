e13afd551d3b66cf8e3f807c4a3961b7b0d5d3b3
Almesberger Marcel <marcel.almesberger@gmail.com>
2020-05-06 07:15:18 +0200

    implement display uploaded file name

--- public/scripts/importexport.js
+++ public/scripts/importexport.js
@@ -1,10 +1,19 @@
 // check if DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM has been loaded and ready!");
-    /*
-    //update #filename element with uploaded file name
-    document.querySelector("#fileUpload").onchange = () => {
-        document.querySelector("#filename").innerHTML = this.files[0].name;
+    
+    // display file name when select file
+    let fileInputs = document.querySelectorAll(".file.has-name");
+    for (let fileInput of fileInputs) {
+        let input = fileInput.querySelector(".file-input");
+        let name = fileInput.querySelector(".file-name");
+        input.addEventListener("change", () => {
+            let files = input.files;
+            if (files.length === 0) {
+                name.innerText = "Keine Datei ausgewählt";
+            } else {
+                name.innerText = files[0].name;
+            }
+        });
     }
-    */
 });
